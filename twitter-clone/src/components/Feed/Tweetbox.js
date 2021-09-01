b62e9ebcf59ef893db1ef99ee7d7909aa593836c
morteza80 <morteza.3921@gmail.com>
2021-09-01 20:09:10 +0430

    tweetbox done

--- twitter-clone/src/components/Feed/Tweetbox.js
+++ twitter-clone/src/components/Feed/Tweetbox.js
@@ -1,11 +1,28 @@
-import React from 'react'
-
+import React , {useState} from 'react';
+import './Tweetbox.css';
+import { Avatar, Button } from "@material-ui/core";
 function Tweetbox() {
+    const[tweetMessage , setTweetMessage] = useState("");
+    const[tweetImage , setTweetImage] = useState("");
+
+
     return (
-        <div>
-            
-        </div>
+        <form className="tweetbox">
+            <div className="tweetbox__input">
+                <Avatar  style={{height:'50px' , width:'50px'}} src="https://pbs.twimg.com/profile_images/1157313327867092993/a09TxL_1_400x400.jpg" />
+                <input value={tweetMessage}
+                onChange = { (e) => setTweetMessage(e.target.value) }
+                type="text" placeholder="What's happening?"/>
+            </div>
+            <div className="tweetbox__footer">
+                <input value={tweetImage}
+                onChange = { (e) => setTweetImage(e.target.value) }
+                type="text" placeholder="Optional : Enter the link of pic/gif"/>
+                <Button>Tweet</Button>
+            </div>
+
+        </form>
     )
 }
 
-export default Tweetbox
+export default Tweetbox;
