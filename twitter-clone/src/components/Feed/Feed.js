98529ede957b364f99711819a5259b6993688dd6
morteza80 <morteza.3921@gmail.com>
2021-09-01 06:51:02 +0430

    some change

--- twitter-clone/src/components/Feed/Feed.js
+++ twitter-clone/src/components/Feed/Feed.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import './Feed.css'
 import Tweetbox from './Tweetbox'
+import {WiStars} from 'react-icons/wi';
 function Feed() {
     return (
         <div className="feed">
+            <div className="feed__header">
+                <h2>Home</h2>
+                <WiStars className="wistars"/>
+            </div>
             <h1>feed</h1>
             <Tweetbox/>
         </div>
--- twitter-clone/src/components/Widget/Widget.js
+++ twitter-clone/src/components/Widget/Widget.js
@@ -19,13 +19,19 @@ function Widget() {
             <div className="widget__container">
                 <h2>What's happening</h2>
                 <TwitterTweetEmbed tweetId={"1432251744508014592"}/>
-                <TwitterTimelineEmbed
-                sourceType="profile"
-                screenName="Cristiano"
-                options={{height: 400}}
-                />
 
             </div>
+            <div className="widget__container2">
+                <div>
+                    <TwitterTimelineEmbed className="hi2"
+                        sourceType="profile"
+                        screenName="Cristiano"
+                        options={{height: 400}}
+                    />
+                </div>
+
+            </div>
+
         </div>
     )
 }
