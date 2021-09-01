e8438d8bd4a36dc72cbef074a60b9d10bb54e4f9
morteza80 <morteza.3921@gmail.com>
2021-09-01 04:38:42 +0430

    widget done and starting feed

--- twitter-clone/src/components/Feed/Feed.js
+++ twitter-clone/src/components/Feed/Feed.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import './Feed.css'
+import Tweetbox from './Tweetbox'
 function Feed() {
     return (
         <div className="feed">
             <h1>feed</h1>
+            <Tweetbox/>
         </div>
     )
 }
--- twitter-clone/src/components/Widget/Widget.js
+++ twitter-clone/src/components/Widget/Widget.js
@@ -1,9 +1,31 @@
 import React from 'react'
 import './Widget.css'
+import SearchIcon from "@material-ui/icons/Search";
+import {
+    TwitterTimelineEmbed,
+    TwitterShareButton,
+    TwitterTweetEmbed,
+    TwitterMomentShare
+  } from "react-twitter-embed";
+
 function Widget() {
     return (
         <div className="widget">
-            <h1>widget</h1>
+            <div className='widget__top'>
+                <SearchIcon className="widget__searchicon"/>
+                <input className="widget__input" type="text" placeholder="Search Twitter"/>
+            </div>
+
+            <div className="widget__container">
+                <h2>What's happening</h2>
+                <TwitterTweetEmbed tweetId={"1432251744508014592"}/>
+                <TwitterTimelineEmbed
+                sourceType="profile"
+                screenName="Cristiano"
+                options={{height: 400}}
+                />
+
+            </div>
         </div>
     )
 }
